e271959481d56365fa8ffa91a4693cd22a9cd127
kanliuxingyudexiatian <1159908634@qq.com>
2017-04-19 09:52:52 +0800

    今天新建的第一个分支。

--- app/src/main/java/com/example/piaohu/firstgitpractice/MainActivity.java
+++ app/src/main/java/com/example/piaohu/firstgitpractice/MainActivity.java
@@ -11,8 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String s = "修改修改修改";
-        String s2="建分支之后增加的";
-        String s3="又一次提交";
-        String s4="新的一个分支";
+        String s2 = "建分支之后增加的";
+        String s3 = "又一次提交";
+        String s4 = "新的一个分支";
+        String s5 = "今天新建的第一个分支";
     }
 }
